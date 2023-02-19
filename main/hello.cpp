dbea243ddfc781ffc179532d00998c382cc9c309
PavankumarHegde <pavankumarkanni@gmail.com>
2023-02-19 16:28:04 +0530

    pes1ug20cs823 commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Pavankumar Hegde\n";
+  cout << "Hello, PES1UG20CS823\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
