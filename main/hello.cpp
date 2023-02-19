b9b5442332cc7131b93ceb784a24305232eafffd
PavankumarHegde1 <125798998+PavankumarHegde1@users.noreply.github.com>
2023-02-19 17:35:22 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(void) {
-  cout << "Pavankumar Hegde\n";
-  cout << "Hello, PES1UG20CS823\n";
+  cout << "Hello, Im Pavankumar Hegde\n";
+  cout << "My SRN is PES1UG20CS823\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
